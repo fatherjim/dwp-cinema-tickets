353eb3cdc81b15d2bea50cebfb5c248af2f440c0
Marios Papadimitri <marios.papadimitri@gmail.com>
2024-10-25 16:16:13 +0100

    Add prompts for number of tickets

--- cinema-tickets-javascript/src/main.js
+++ cinema-tickets-javascript/src/main.js
@@ -1,3 +1,52 @@
+import readline from 'readline';
+
 import strings from './pairtest/utils/strings.json' with { type: 'json' };
 
 console.log(strings.welcome);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let adultTicketsNo;
+let childTicketsNo;
+let infantTicketsNo;
+
+const adultTicketsQuestion = () => {
+  return new Promise((resolve, _) => {
+    rl.question(strings.request_number_of_adult_tickets, (answer) => {
+      adultTicketsNo = answer;
+      resolve();
+    });
+  });
+};
+const childTicketsQuestion = () => {
+  return new Promise((resolve, _) => {
+    rl.question(strings.request_number_of_child_tickets, (answer) => {
+      childTicketsNo = answer;
+      resolve();
+    });
+  });
+};
+const infantTicketsQuestion = () => {
+  return new Promise((resolve, _) => {
+    rl.question(strings.request_number_of_infant_tickets, (answer) => {
+      infantTicketsNo = answer;
+      resolve();
+    });
+  });
+};
+
+const main = async () => {
+  await adultTicketsQuestion();
+  await childTicketsQuestion();
+  await infantTicketsQuestion();
+  rl.close();
+
+  console.log(`Adult tickets: ${adultTicketsNo}`);
+  console.log(`Child tickets: ${childTicketsNo}`);
+  console.log(`Infant tickets: ${infantTicketsNo}`);
+};
+
+main();
